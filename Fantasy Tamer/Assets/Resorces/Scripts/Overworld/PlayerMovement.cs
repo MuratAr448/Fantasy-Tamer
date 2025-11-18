647bc762bff3426cec367efe8b25c50f70d8dd51
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2025-11-18 13:40:36 +0100

    Player animations

--- Fantasy Tamer/Assets/Resorces/Scripts/Overworld/PlayerMovement.cs
+++ Fantasy Tamer/Assets/Resorces/Scripts/Overworld/PlayerMovement.cs
@@ -1,14 +1,54 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+
+    Vector2 Idle;
+    public float moveSpeed = 5f;
+    public Rigidbody2D RB2D;
+    public Animator animator;
+    Vector2 movement;
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    GameObject campos;
+    float Dis = 0;
 
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.magnitude);
+        if (movement.magnitude > 0.1f)
+        {
+            Idle = movement;
+            animator.SetFloat("Idle_H", Idle.x);
+            animator.SetFloat("Idle_V", Idle.y);
+        }
+
+
+
+
+    }
+    void CamFollow()
+    {
+        Dis = Vector3.Distance(cam.transform.position, campos.transform.position);
+        if (Dis > 3 && cam.transform.position != campos.transform.position)
+        {
+            cam.transform.position = Vector3.MoveTowards(cam.transform.position, campos.transform.position, 8 * Time.deltaTime);
+        }
+        else
+        if (cam.transform.position != campos.transform.position)
+        {
+            cam.transform.position = Vector3.MoveTowards(cam.transform.position, campos.transform.position, 3 * Time.deltaTime);
+        }
+    }
+    private void FixedUpdate()
+    {
+        RB2D.MovePosition(RB2D.position + movement * moveSpeed * Time.fixedDeltaTime);
+        CamFollow();
     }
 }
